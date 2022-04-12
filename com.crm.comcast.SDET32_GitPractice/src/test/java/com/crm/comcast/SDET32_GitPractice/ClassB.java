ecf3b1b5e27352a37e5c8f7ae3cb71f20b1a93d5
sinchan <sinchan@Mukul-1515>
2022-04-12 12:24:46 +0530

    commit 2

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassB.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+
+	@Test
+	public void demoTest2() {
+		
+		System.out.println("puch 2");
+		System.out.println("puch 3");
+	}
+}
