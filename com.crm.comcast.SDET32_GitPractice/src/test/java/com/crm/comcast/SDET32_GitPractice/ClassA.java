6d5d27a716c81a90524bb0d5416cc3c2133edc59
sinchan <sinchan@Mukul-1515>
2022-04-12 11:37:25 +0530

    Commit 1

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassA.java
@@ -0,0 +1,12 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+
+	@Test
+	public void demoTset() {
+		
+		System.out.println("FirstPuch");
+	}
+}
